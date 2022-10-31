7b3d24c4fb243671161b772758bd8e2cf0f005fe
Joey deVilla <joey@joeydevilla.com>
2022-10-31 15:07:01 -0400

    Initial commit

---
+++ JetpackComposeLogin/app/src/main/java/com/auth0/jetpackcomposelogin/MainActivity.kt
@@ -0,0 +1,194 @@
+package com.auth0.jetpackcomposelogin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import com.auth0.jetpackcomposelogin.ui.theme.JetpackComposeLoginTheme
+
+
+class MainActivity : ComponentActivity() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mainViewModel.setContext(this)
+
+        setContent {
+            JetpackComposeLoginTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MainView(mainViewModel)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainView(
+    viewModel: MainViewModel
+) {
+    Column(
+        modifier = Modifier.padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        // Title
+        // -----
+        val title = if (viewModel.userIsAuthenticated) {
+            stringResource(R.string.logged_in_title)
+        } else {
+            if (viewModel.appJustLaunched) {
+                stringResource(R.string.initial_title)
+            } else {
+                stringResource(R.string.logged_out_title)
+            }
+        }
+        Title(
+            text = title
+        )
+
+        // User info
+        // ---------
+        if (viewModel.userIsAuthenticated) {
+            UserInfoRow(
+                label = stringResource(R.string.name_label),
+                value = viewModel.user.name,
+            )
+            UserInfoRow(
+                label = stringResource(R.string.email_label),
+                value = viewModel.user.email,
+            )
+            UserPicture(
+                url = viewModel.user.picture,
+                description = viewModel.user.name,
+            )
+        }
+
+        // Button
+        // ------
+        val buttonText: String
+        val onClickAction: () -> Unit
+        if (viewModel.userIsAuthenticated) {
+            buttonText = stringResource(R.string.log_out_button)
+            onClickAction = { viewModel.logout() }
+        } else {
+            buttonText = stringResource(R.string.log_in_button)
+            onClickAction = { viewModel.login() }
+        }
+        LogButton(
+            text = buttonText,
+            onClick = onClickAction,
+        )
+    }
+}
+
+@Composable
+fun Title(
+    text: String,
+)
+{
+    Text(
+        text = text,
+        style = TextStyle(
+            fontFamily = FontFamily.Default,
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+        )
+    )
+}
+
+@Composable
+fun UserInfoRow(
+    label: String,
+    value: String,
+) {
+    Row {
+        Text(
+            text = label,
+            style = TextStyle(
+                fontFamily = FontFamily.Default,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+            )
+        )
+        Spacer(
+            modifier = Modifier.width(10.dp),
+        )
+        Text(
+            text = value,
+            style = TextStyle(
+                fontFamily = FontFamily.Default,
+                fontSize = 20.sp,
+            )
+        )
+    }
+}
+
+@Composable
+fun UserPicture(
+    url: String,
+    description: String,
+) {
+    Column(
+        modifier = Modifier
+            .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(url),
+            contentDescription = description,
+            modifier = Modifier
+                .fillMaxSize(0.5f),
+        )
+    }
+}
+
+@Composable
+fun LogButton(
+    text: String,
+    onClick: () -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Button(
+            onClick = { onClick() },
+            modifier = Modifier
+                .width(200.dp)
+                .height(50.dp),
+        ) {
+            Text(
+                text = text,
+                fontSize = 20.sp,
+            )
+        }
+    }
+}
